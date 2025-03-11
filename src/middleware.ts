2f6d436648626d3230b18d3fb4492b6fb3bd3de8
Bilha <bilhamwengi34@gmail.com>
2025-03-11 20:12:18 +0300

    meal plan page and ai integration

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,41 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/", "/sign-up(.*)", "/subscribe(.*)", "/api/webhook(.*)"
+  "/", 
+  "/sign-up(.*)", 
+  "/subscribe(.*)", 
+  "/api/webhook(.*)",
+  "/api/check-subscription(.*)"
 ])
 
 const isSignUpRoute = createRouteMatcher(["/sign-up(.*)"]);
+const isMealPlanRoute = createRouteMatcher(["/mealplan(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const userAuth = await auth();
   const {userId} = userAuth;
-  const {origin} = req.nextUrl;
+  const {pathname, origin} = req.nextUrl;
+
+  if(pathname === "/api/check-subscription") {
+    return NextResponse.next();
+  }
 
   if(!isPublicRoute(req) && !userId) {
     return NextResponse.redirect(new URL("/sign-up", origin))
   }
   if(isSignUpRoute(req) && userId) {
     return NextResponse.redirect(new URL("/mealplan", origin))
+  }
+  if(isMealPlanRoute(req) && userId) {
+    try {
+      const response = await fetch(`${origin}/api/check-subscription?userId=${userId}`);
+      const data = await response.json();
+      if(!data.subscriptionActive){
+        return NextResponse.redirect(new URL("/subscribe", origin));
+      }
+    } catch(error: any) {
+      return NextResponse.redirect(new URL("/subscribe", origin));
+    }
   }
   return NextResponse.next();
 });
